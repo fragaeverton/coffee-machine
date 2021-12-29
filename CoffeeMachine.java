cde282f9b9eaceb8e89388b07e4a7e7a7d5acfc3
fragaeverton <evertonfraga90@hotmail.com>
2021-12-29 15:36:11 +0000

    5 / 6

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -8,6 +8,11 @@ public class CoffeeMachine {
     final static int WATER_RECIPE = 200;
     final static int MILK_RECIPE = 50;
     final static int COFFEE_BEANS_RECIPE = 15;
+    final static String MSG_OUT_OF_WATER = "Sorry, not enough water!";
+    final static String MSG_OUT_OF_MILK = "Sorry, not enough milk!";
+    final static String MSG_OUT_OF_COFFEE_BEANS = "Sorry, not enough coffee beans!";
+    final static String MSG_OUT_OF_CUPS = "Sorry, not enough disposable cups!";
+    final static String MSG_ENOUGH_RESOURCES = "I have enough resources, making you a coffee!";
     static int waterStock;
     static int milkStock;
     static int coffeeBeansStock;
@@ -20,15 +25,13 @@ public class CoffeeMachine {
         coffeeBeansStock = 120;
         disposableCupsStock = 9;
         moneyAmount = 550;
-        showSupply();
         showMenu();
     }
 
     private static void showMenu(){
         System.out.println();
-        System.out.println("Write action (buy, fill, take): ");
-        String action = scanner.nextLine();
-        switch (action){
+        System.out.println("Write action (buy, fill, take, remaining, exit): ");
+        switch (scanner.nextLine()){
             case "buy" :
                 buyCoffee();
                 break;
@@ -38,55 +41,127 @@ public class CoffeeMachine {
             case "take" :
                 takeMoney();
                 break;
+            case "remaining" :
+                remaining();
+                break;
+            case "exit" :
+                break;
         }
     }
 
     private static void buyCoffee() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        switch (scanner.nextInt()){
-            case 1 :
-                waterStock -= 250;
-                coffeeBeansStock -= 16;
-                disposableCupsStock -= 1;
-                moneyAmount += 4;
+        System.out.println();
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        switch (scanner.nextLine()){
+            case "1" :
+                if (waterStock >= 250) {
+                    if (coffeeBeansStock >= 16) {
+                        if (disposableCupsStock >= 1) {
+                            System.out.println(MSG_ENOUGH_RESOURCES);
+                            waterStock -= 250;
+                            coffeeBeansStock -= 16;
+                            disposableCupsStock -= 1;
+                            moneyAmount += 4;
+                            showMenu();
+                        } else {
+                            System.out.println(MSG_OUT_OF_CUPS);
+                            showMenu();
+                        }
+                    } else {
+                        System.out.println(MSG_OUT_OF_COFFEE_BEANS);
+                        showMenu();
+                    }
+                } else {
+                    System.out.println(MSG_OUT_OF_WATER);
+                    showMenu();
+                }
                 break;
-            case 2 :
-                waterStock -= 350;
-                milkStock -= 75;
-                coffeeBeansStock -= 20;
-                disposableCupsStock -= 1;
-                moneyAmount += 7;
+            case "2" :
+                if (waterStock >= 350) {
+                    if (milkStock >= 75) {
+                        if (coffeeBeansStock >= 20) {
+                            if (disposableCupsStock >= 1) {
+                                System.out.println(MSG_ENOUGH_RESOURCES);
+                                waterStock -= 350;
+                                milkStock -= 75;
+                                coffeeBeansStock -= 20;
+                                disposableCupsStock -= 1;
+                                moneyAmount += 7;
+                                showMenu();
+                            } else {
+                                System.out.println(MSG_OUT_OF_CUPS);
+                                showMenu();
+                            }
+                        } else {
+                            System.out.println(MSG_OUT_OF_COFFEE_BEANS);
+                            showMenu();
+                        }
+                    } else {
+                        System.out.println(MSG_OUT_OF_MILK);
+                        showMenu();
+                    }
+                } else {
+                    System.out.println(MSG_OUT_OF_WATER);
+                    showMenu();
+                }
                 break;
-            case 3 :
-                waterStock -= 200;
-                milkStock -= 100;
-                coffeeBeansStock -= 12;
-                disposableCupsStock -= 1;
-                moneyAmount += 6;
+            case "3" :
+                if (waterStock >= 200) {
+                    if (milkStock >= 100) {
+                        if (coffeeBeansStock >= 12) {
+                            if (disposableCupsStock >= 1) {
+                                System.out.println(MSG_ENOUGH_RESOURCES);
+                                waterStock -= 200;
+                                milkStock -= 100;
+                                coffeeBeansStock -= 12;
+                                disposableCupsStock -= 1;
+                                moneyAmount += 6;
+                                showMenu();
+                            } else {
+                                System.out.println(MSG_OUT_OF_CUPS);
+                                showMenu();
+                            }
+                        } else {
+                            System.out.println(MSG_OUT_OF_COFFEE_BEANS);
+                            showMenu();
+                        }
+                    } else {
+                        System.out.println(MSG_OUT_OF_MILK);
+                        showMenu();
+                    }
+                } else {
+                    System.out.println(MSG_OUT_OF_WATER);
+                    showMenu();
+                }
+                break;
+            case "back" :
+                showMenu();
                 break;
         }
-        showSupply();
     }
 
     private static void fillSupply() {
+        System.out.println();
         System.out.println("Write how many ml of water you want to add: ");
-        waterStock += scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        waterStock += sc.nextInt();
         System.out.println("Write how many ml of milk you want to add: ");
-        milkStock += scanner.nextInt();
+        milkStock += sc.nextInt();
         System.out.println("Write how many grams of coffee beans you want to add: ");
-        coffeeBeansStock += scanner.nextInt();
+        coffeeBeansStock += sc.nextInt();
         System.out.println("Write how many disposable cups of coffee you want to add: ");
-        disposableCupsStock += scanner.nextInt();
-        showSupply();
+        disposableCupsStock += sc.nextInt();
+        showMenu();
     }
 
     private static void takeMoney() {
+        System.out.println();
         System.out.println("I gave you $" + moneyAmount);
         moneyAmount = 0;
-        showSupply();
+        showMenu();
     }
 
-    private static void showSupply(){
+    private static void remaining(){
         System.out.println();
         System.out.println("The coffee machine has: ");
         System.out.println(waterStock + " ml of water");
@@ -94,20 +169,10 @@ public class CoffeeMachine {
         System.out.println(coffeeBeansStock + " g of coffee beans");
         System.out.println(disposableCupsStock + " disposable cups");
         System.out.println("$" + moneyAmount + " of money");
+        showMenu();
     }
 
-    private static void checkSupply() {
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int qttWater = scanner.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int qttMilk = scanner.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        int qttCoffee = scanner.nextInt();
-        checkRecipe(qttWater, qttMilk, qttCoffee);
-
-    }
-
-    private static void checkRecipe(int qttWater, int qttMilk, int qttCoffee) {
+    private static void checkSupply(int qttWater, int qttMilk, int qttCoffee) {
         System.out.println("Write how many cups of coffee you will need: ");
         int qttCups = scanner.nextInt();
         int availableCups = qttWater;
@@ -125,26 +190,5 @@ public class CoffeeMachine {
             System.out.println("Yes, I can make that amount of coffee (and even " + (availableCups - qttCups) + " more than that)");
         }
 
-    }
-
-    private static void orderCoffee() {
-        System.out.println("Write how many cups of coffee you will need: ");
-        int qttCups = scanner.nextInt();
-
-        String[] myRecipe = getRecipeNeeds(qttCups);
-        System.out.printf("For %d cups of coffee you will need: ", qttCups);
-        System.out.println();
-        for (String ingredient : myRecipe) {
-            System.out.println(ingredient);
-        }
-    }
-
-    private static String[] getRecipeNeeds(int qttCups) {
-
-        return new String[]{
-                WATER_RECIPE * qttCups + " ml of water",
-                MILK_RECIPE * qttCups + " ml of milk",
-                COFFEE_BEANS_RECIPE * qttCups + " g of coffee beans"
-        };
     }
 }
